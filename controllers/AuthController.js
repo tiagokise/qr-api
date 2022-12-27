03bb1631430a0f4fe1ff8468bc27435b5d1c008f
Gustavo Garcia <gustavo.ogarcia@gmail.com>
2022-12-27 19:47:52 -0300

    to creck

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -12,86 +12,51 @@ const { constants } = require("../helpers/constants");
 /**
  * User registration.
  *
- * @param {string}      firstName
- * @param {string}      lastName
- * @param {string}      email
- * @param {string}      password
+ * @param {string} userFullName
+ * @param {string} email
+ * @param {string} password
  *
  * @returns {Object}
  */
 exports.register = [
-	// Validate fields.
-	body("firstName").isLength({ min: 1 }).trim().withMessage("First name must be specified.")
-		.isAlphanumeric().withMessage("First name has non-alphanumeric characters."),
-	body("lastName").isLength({ min: 1 }).trim().withMessage("Last name must be specified.")
-		.isAlphanumeric().withMessage("Last name has non-alphanumeric characters."),
-	body("email").isLength({ min: 1 }).trim().withMessage("Email must be specified.")
-		.isEmail().withMessage("Email must be a valid email address.").custom((value) => {
+	body("userFullName")
+		.isLength({ min: 3 }).trim().withMessage("Nome completo é obrigatório."),
+
+	body("email")
+		.isLength({ min: 3 }).trim().withMessage("Email é obrigatório.")
+		.isEmail().withMessage("Por favor insira um email válido.").custom((value) => {
 			return UserModel.findOne({email : value}).then((user) => {
-				if (user) {
-					return Promise.reject("E-mail already in use");
-				}
+				if (user) { return Promise.reject("E-mail já está em uso"); }
 			});
 		}),
-	body("password").isLength({ min: 6 }).trim().withMessage("Password must be 6 characters or greater."),
-	// Sanitize fields.
-	sanitizeBody("firstName").escape(),
-	sanitizeBody("lastName").escape(),
+
+	body("password").isLength({ min: 6 }).trim().withMessage("A Senha deve ter no minímo 6 caracteres."),
+
+	sanitizeBody("userFullName").escape(),
 	sanitizeBody("email").escape(),
 	sanitizeBody("password").escape(),
-	// Process request after validation and sanitization.
+
 	(req, res) => {
 		try {
-			// Extract the validation errors from a request.
 			const errors = validationResult(req);
-			if (!errors.isEmpty()) {
-				// Display sanitized values/errors messages.
-				return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array());
-			}else {
-				//hash input password
-				bcrypt.hash(req.body.password,10,function(err, hash) {
-					// generate OTP for confirmation
-					let otp = utility.randomNumber(4);
-					// Create User object with escaped and trimmed data
-					var user = new UserModel(
-						{
-							firstName: req.body.firstName,
-							lastName: req.body.lastName,
-							email: req.body.email,
-							password: hash,
-							confirmOTP: otp
-						}
-					);
-					// Html email body
-					let html = "<p>Please Confirm your Account.</p><p>OTP: "+otp+"</p>";
-					// Send confirmation email
-					mailer.send(
-						constants.confirmEmails.from, 
-						req.body.email,
-						"Confirm Account",
-						html
-					).then(function(){
-						// Save user.
-						user.save(function (err) {
-							if (err) { return apiResponse.ErrorResponse(res, err); }
-							let userData = {
-								_id: user._id,
-								firstName: user.firstName,
-								lastName: user.lastName,
-								email: user.email
-							};
-							return apiResponse.successResponseWithData(res,"Registration Success.", userData);
-						});
-					}).catch(err => {
-						console.log(err);
-						return apiResponse.ErrorResponse(res,err);
-					}) ;
-				});
-			}
-		} catch (err) {
-			//throw error in json response with status 500.
-			return apiResponse.ErrorResponse(res, err);
-		}
+			if (!errors.isEmpty()) { return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array()); }
+			bcrypt.hash(req.body.password, 10, function(err, hash) {
+				let otp = utility.randomNumber(4);
+        var user = new UserModel({ userFullName: req.body.userFullName, email: req.body.email, password: hash, confirmOTP: otp });
+        // let html = "<p>Por favor comfirme seu email.</p><p>Código: "+otp+"</p>";
+        user.save(function (err) {
+          if (err) { return apiResponse.ErrorResponse(res, err); }
+          let userData = { _id: user._id, firstName: user.firstName, lastName: user.lastName, email: user.email };
+          return apiResponse.successResponseWithData(res,"Registration Success.", userData);
+        });
+        // mailer.send(constants.confirmEmails.from, req.body.email, "Confirm Account", html)
+        //   .then(() => {
+        //   }).catch(err => {
+        //     console.log(err);
+        //     return apiResponse.ErrorResponse(res,err);
+        //   });
+      });
+    } catch (err) { return apiResponse.ErrorResponse(res, err); }
 	}];
 
 /**
